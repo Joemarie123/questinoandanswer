bd249e8422cd7568b327c1a82b311b3e1a2f9a5a
marj28 <honeymarjey@gmail.com>
2023-08-02 08:31:17 +0800

    Adding Reading Comprehension

--- src/components/SetBDisplayQuestion.vue
+++ src/components/SetBDisplayQuestion.vue
@@ -1,52 +1,70 @@
 <template>
-    <div class="questions-ctr">
-      <v-card  :elevation="isHovering ? 24 : 6">
-          <p class="mx-3 my-3" style="font-size:17px"><strong>General Instruction:</strong> Write your answer on the separated ANSWER SHEET. You have 2 ½ hours to finish the test. STRICTLY NO ERASURES AND ALTERATION ALLOWED. DO NOT WRITE ANYTHING ON THIS TEST QUESTIONNAIRE. </p>
-        </v-card>
-      <div class="progress">
-        <div
-          class="bar"
-          :style="{ width: `${(questionsAnswered / questions.length) * 100}%` }"
-        ></div>
-       
-        <div class="status">
-          <span style="color:Black">GENERAL INFORMATION</span> {{ questions.length }} questions answered
-        </div>
+  <div class="questions-ctr">
+    <v-card  :elevation="isHovering ? 24 : 6">
+        <p class="mx-3 my-3" style="font-size:17px"><strong>General Instruction:</strong> Write your answer on the separated ANSWER SHEET. You have 2 ½ hours to finish the test. STRICTLY NO ERASURES AND ALTERATION ALLOWED. DO NOT WRITE ANYTHING ON THIS TEST QUESTIONNAIRE. </p>
+      </v-card>
+    <div class="progress">
+      <div
+        class="bar"
+        :style="{ width: `${(questionsAnswered / questions.length) * 100}%` }"
+      ></div>
+      
+      
+      <div class="status">
+         {{ questions.length }} questions answered
       </div>
-      <transition-group name="fade">
-        <div
-          class="single-question"
-          v-for="(question, qi) in questions"
-          :key="question.q"
-          v-show="questionsAnswered === qi"
-        >
-  
-         <!--  <div class="question"> -->
-         <p class="my-5 mx-1">   {{ question.q }}</p>
-          <!-- </div> -->
-          <div class="answers">
-            <div
-              class="answer"
-              v-for="answer in question.answers"
-              :key="answer.text"
-              @click.prevent="selectAnswer(answer.is_correct)"
-            >
-              {{ answer.text }}
-            </div>
+    </div>
+      
+    <transition-group name="fade">
+      <div
+        class="single-question"
+        v-for="(question, qi) in questions"
+        :key="question.q"
+        v-show="questionsAnswered === qi"
+      >
+      <p class="my-5 mx-1"> <strong>{{ question.subject }}</strong> </p>
+      <p class="mt-n4 mx-5" style="font-size:15px"> <strong>{{ question.direction }}</strong> {{ question.displaydirection }} </p>
+       <!--  <div class="question"> -->
+       <p class="my-5 mx-1">   {{ question.q }}</p>
+    
+
+        <!-- </div> -->
+        <div class="answers">
+          <div
+            class="answer"
+            v-for="answer in question.answers"
+            :key="answer.text"
+            @click.prevent="selectAnswer(answer.is_correct)"
+          >
+            {{ answer.text }}
           </div>
         </div>
-      </transition-group>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    props: ["questions", "questionsAnswered"],
-    emits: ["question-answered"],
-    methods: {
-      selectAnswer(is_correct) {
-        this.$emit("question-answered", is_correct);
-      },
+      </div>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["questions", "questionsAnswered"],
+  emits: ["question-answered"],
+  methods: {
+    
+       
+       
+    selectAnswer(is_correct) {
+      this.$emit("question-answered", is_correct);
     },
-  };
-  </script>
+
+  },
+
+ /*  mounted:{
+
+  
+
+
+  } */
+
+
+};
+</script>
