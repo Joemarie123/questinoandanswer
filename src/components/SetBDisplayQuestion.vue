7fca010e5bd12151c42b775c73ea5c416762fdbf
marj28 <honeymarjey@gmail.com>
2023-08-03 16:34:35 +0800

    Adding class

--- src/components/SetBDisplayQuestion.vue
+++ src/components/SetBDisplayQuestion.vue
@@ -1,49 +1,72 @@
 <template>
   <div class="questions-ctr">
     <v-card class="mx-3 mx-lg-0" :elevation="isHovering ? 24 : 6">
-        <p class="mx-3 my-3" style="font-size:17px; text-align: justify;"><strong>General Instruction:</strong> Welcome to the Expanded-City Educational Scholarship Program (E-CESPRO) online examination for SY 2022-2023. Please carefully read and choose the correct answer for each item. The exam consists of 65 questions and one essay type. The time allocation for this exam is only 2 hours, plus an additional 30 minutes for your essay. Warning: Attempting to exit the exam interface or open other browser tabs will automatically exit your examination.</p>
-      </v-card>
-    <div class="progress">
-      <div
+      <p class="mx-3 my-3" style="font-size: 15px; text-align: justify">
+        <strong>General Instruction:</strong> Welcome to the Expanded-City
+        Educational Scholarship Program (E-CESPRO) online examination for SY
+        2022-2023. Please carefully read and choose the correct answer for each
+        item. The exam consists of 65 questions and one essay type. The time
+        allocation for this exam is only 2 hours, plus an additional 30 minutes
+        for your essay. Warning: Attempting to exit the exam interface or open
+        other browser tabs will automatically exit your examination.
+      </p>
+    </v-card>
+    <div class="progress mx-3 mx-lg-0">
+      <v-card
         class="bar"
         :style="{ width: `${(questionsAnswered / questions.length) * 100}%` }"
-      ></div>
-      
-      
-      <div class="status">
-         {{ questions.length }} questions answered
-      </div>
+      ></v-card>
+      <div class="status">{{ questions.length }} questions answered</div>
     </div>
-      
+
     <transition-group name="fade">
       <div
-        class="single-question"
+        class="single-question mb-6"
         v-for="(question, qi) in questions"
         :key="question.q"
         v-show="questionsAnswered === qi"
       >
-      <p class="my-5 mx-1"> <strong>{{ question.subject }}</strong> </p>
-      <p class="mt-n4 mx-5" style="font-size:15px; text-align: justify;"> <strong>{{ question.direction }}</strong> {{ question.displaydirection }} </p>
-      <p class="justify mt-2 mx-2" style="font-size:14px"> {{ question.phrase_one }} </p>
-        <p class="justify mt-2 mx-2" style="font-size:14px"> {{ question.phrase_two }} </p>
-        <p class="justify mt-2 mx-2" style="font-size:14px"> {{ question.phrase_three }} </p>
-        <p class="justify mt-2 mx-2" style="font-size:14px"> {{ question.phrase_four }} </p>
-        <p class="justify mt-2 mx-2" style="font-size:14px"> {{ question.phrase_five }} </p>
-      <!--  <div class="question"> -->
-       <p class="my-5 mx-1"> {{ question.number }} <u> {{ question.underline }}</u>  {{ question.q }}  </p>
-    
+        <v-card class="mx-3 mx-lg-0 pa-2">
+          <p class="my-5 mx-1 ">
+            <strong>{{ question.subject }}</strong>
+          </p>
+          <p class="mt-n4 mx-5" style="font-size: 15px; text-align: justify">
+            <strong>{{ question.direction }}</strong>
+            {{ question.displaydirection }}
+          </p>
+          <p class="justify mt-2 mx-2" style="font-size: 14px">
+            {{ question.phrase_one }}
+          </p>
+          <p class="justify mt-2 mx-2" style="font-size: 14px">
+            {{ question.phrase_two }}
+          </p>
+          <p class="justify mt-2 mx-2" style="font-size: 14px">
+            {{ question.phrase_three }}
+          </p>
+          <p class="justify mt-2 mx-2" style="font-size: 14px">
+            {{ question.phrase_four }}
+          </p>
+          <p class="justify mt-2 mx-2" style="font-size: 14px">
+            {{ question.phrase_five }}
+          </p>
+          <!--  <div class="question"> -->
+          <p class="my-5 mx-1">
+            {{ question.number }} <u> {{ question.underline }}</u>
+            {{ question.q }}
+          </p>
 
-        <!-- </div> -->
-        <div class="answers">
-          <div
-            class="answer"
-            v-for="answer in question.answers"
-            :key="answer.text"
-            @click.prevent="selectAnswer(answer.is_correct)"
-          >
-            {{ answer.text }}
+          <!-- </div> -->
+          <div class="answers">
+            <div
+              class="answer"
+              v-for="answer in question.answers"
+              :key="answer.text"
+              @click.prevent="selectAnswer(answer.is_correct)"
+            >
+              {{ answer.text }}
+            </div>
           </div>
-        </div>
+        </v-card>
       </div>
     </transition-group>
   </div>
@@ -54,29 +77,25 @@ export default {
   props: ["questions", "questionsAnswered"],
   emits: ["question-answered"],
   methods: {
-    
-       
-       
     selectAnswer(is_correct) {
       this.$emit("question-answered", is_correct);
     },
-
   },
 
- /*  mounted:{
+  /*  mounted:{
 
   
 
 
   } */
-
-
 };
 </script>
 <style>
 .justify {
   text-align: justify;
   text-justify: inter-word;
-  
+}
+.status {
+  color: green;
 }
 </style>
--- src/views/ExamName.vue
+++ src/views/ExamName.vue
@@ -1,183 +1,184 @@
 <template>
- 
-    <v-app>
+  <v-app class="mx-3 mx-lg-0">
     <v-container >
-     
-       
-      <v-row  justify="center" >
-        
-        <v-col cols="12" >
-            
-          <v-card 
-          
-          :class="`elevation-${isHovering ? 24 : 6}`"
-          class=" login-form " >
-         <!--  <div class="text-center mt-3">
+      <v-row justify="center">
+        <v-col cols="12">
+          <v-card
+            :class="`elevation-${isHovering ? 24 : 6}`"
+            class="login-form"
+          >
+            <!--  <div class="text-center mt-3">
             <img class="logo" src="/public/Tagum.png" width="120">
           </div> -->
-        
-          <v-alert v-if="alertMessage" :value="true" type="success" transition="scale-transition">
-      {{ alertMessage }}
-    </v-alert>
 
-            <p class="my-4 login-title">
-               
-                I'm sending the examination package for E-CESPRO. Also, I would like to include the following before they take their exam. </p>
+            <v-alert
+              v-if="alertMessage"
+              :value="true"
+              type="success"
+              transition="scale-transition"
+            >
+              {{ alertMessage }}
+            </v-alert>
+
+            <p class="my-4 login-title pa-4">
+              I'm sending the examination package for E-CESPRO. Also, I would
+              like to include the following before they take their exam.
+            </p>
             <v-card-text>
-             
-                
               <v-text-field
                 class="mx-2"
                 prepend-inner-icon="mdi-account"
-               density="compact" 
-               variant="outlined"
-              v-model="fullname"
-             label="Full Name"
-                
-                >
+                density="compact"
+                variant="outlined"
+                v-model="fullname"
+                label="Full Name"
+              >
+              </v-text-field>
 
-                </v-text-field>
-                
-                <v-select
+              <v-select
                 class="mx-2"
-                density="compact" 
-               variant="outlined"
-               v-model="selectbarangay"
-            :items="['Apokon', 'Bincungan', 'Busaon', 'Canocotan', 'Cuambogan', 'La Filipina', 'Liboganon', 'Madaum', 'Magdum', 'Mankilam', 'New Balamban', 'Nueva Fuerza', 'Pagsabangan', 'Pandapan', 'Magugpo Poblacion', 'San Agustin', 'San Isidro', 'San Miguel (Camp 4)', 'Visayan Village', 'Magugpo East', 'Magugpo North', 'Magugpo South', 'Magugpo West']"
-             label="Select Barangay" prepend-inner-icon="mdi-map-marker"
-             required></v-select>
+                density="compact"
+                variant="outlined"
+                v-model="selectbarangay"
+                :items="[
+                  'Apokon',
+                  'Bincungan',
+                  'Busaon',
+                  'Canocotan',
+                  'Cuambogan',
+                  'La Filipina',
+                  'Liboganon',
+                  'Madaum',
+                  'Magdum',
+                  'Mankilam',
+                  'New Balamban',
+                  'Nueva Fuerza',
+                  'Pagsabangan',
+                  'Pandapan',
+                  'Magugpo Poblacion',
+                  'San Agustin',
+                  'San Isidro',
+                  'San Miguel (Camp 4)',
+                  'Visayan Village',
+                  'Magugpo East',
+                  'Magugpo North',
+                  'Magugpo South',
+                  'Magugpo West',
+                ]"
+                label="Select Barangay"
+                prepend-inner-icon="mdi-map-marker"
+                required
+              ></v-select>
 
-            
-                
-
-                <v-text-field
+              <v-text-field
                 class="mx-2"
                 prepend-inner-icon="mdi-account"
-               density="compact" 
-               variant="outlined"
-              v-model="incomingyearlevel"
-             label="Incoming Year Level"
-                
-                >
+                density="compact"
+                variant="outlined"
+                v-model="incomingyearlevel"
+                label="Incoming Year Level"
+              >
+              </v-text-field>
 
-                </v-text-field>
-                
-
-                <v-text-field
+              <v-text-field
                 class="mx-2"
                 prepend-inner-icon="mdi-account"
-               density="compact" 
-               variant="outlined"
-              v-model="schoolintendedtoenroll"
-             label="School Intended To Enroll"
-                
-                >
+                density="compact"
+                variant="outlined"
+                v-model="schoolintendedtoenroll"
+                label="School Intended To Enroll"
+              >
+              </v-text-field>
 
-                </v-text-field>
-                
-<div class="ml-4">
+              <div class="ml-4">
                 <h4>SELECT TYPE OF EXAM</h4>
-                <v-radio-group  v-model="selectedSet"   inline>
-
-  <v-radio label="SET A" value="setA"></v-radio>
-  <v-radio label="SET B"  value="setB"></v-radio>
-
-</v-radio-group>
-</div>
-                <v-row>
-                    <v-col cols="12">
-                <v-btn type="submit"  color="primary" @click="submitForm()"  class="ml-2 ">
-                  SUBMIT
-                </v-btn>
-            </v-col>
-            
-            </v-row>
-          
-             
+                <v-radio-group v-model="selectedSet" inline>
+                  <v-radio label="SET A" value="setA"></v-radio>
+                  <v-radio label="SET B" value="setB"></v-radio>
+                </v-radio-group>
+              </div>
+              <v-row>
+                <v-col cols="12">
+                  <v-btn
+                    type="submit"
+                    color="green"
+                    @click="submitForm()"
+                    class="ml-2"
+                  >
+                    Continue
+                  </v-btn>
+                </v-col>
+              </v-row>
             </v-card-text>
           </v-card>
         </v-col>
       </v-row>
     </v-container>
-</v-app>
-  </template>
+  </v-app>
+</template>
   
   <script>
-  export default {
-    data() {
-    return{
-        fullname:'',
-        selectbarangay:'',
-        incomingyearlevel:'',
-        schoolintendedtoenroll:'',
-        selectedSet: '',
-        alertMessage: '', // Variable to store the alert message
-    }
-    },
-    methods: {
- 
-      submitForm() {
-      if (this.selectedSet === 'setA') {
+export default {
+  data() {
+    return {
+      fullname: "",
+      selectbarangay: "",
+      incomingyearlevel: "",
+      schoolintendedtoenroll: "",
+      selectedSet: "",
+      alertMessage: "", // Variable to store the alert message
+    };
+  },
+  methods: {
+    submitForm() {
+      if (this.selectedSet === "setA") {
         // Replace 'FormA' with the name of the component or route to the form you want to transfer to for Set A
-        this.$router.push({ name: 'SetAExamList' });
-      } else if (this.selectedSet === 'setB') {
+        this.$router.push({ name: "SetAExamList" });
+      } else if (this.selectedSet === "setB") {
         // Replace 'FormB' with the name of the component or route to the form you want to transfer to for Set B
-        this.$router.push({ name: 'SetBExamList' });
+        this.$router.push({ name: "SetBExamList" });
       } else {
         // Handle invalid selection or show an error message
-        this.alertMessage = 'Please select either SET A or SET B before submitting.';
+        this.alertMessage =
+          "Please select either SET A or SET B before submitting.";
         setTimeout(() => {
-          this.alertMessage = ''; // Clear the alert message after a few seconds
+          this.alertMessage = ""; // Clear the alert message after a few seconds
         }, 5000);
       }
     },
 
-
-      Login_Events()
-  {
-
-    if(this.usernamenako=="hr",this.password=="hr")
-    {
-
-       this.$router.push('/HomeEvents'); 
+    Login_Events() {
+      if ((this.usernamenako == "hr", this.password == "hr")) {
+        this.$router.push("/HomeEvents");
+      } else if ((this.usernamenako == "admin", this.password == "admin")) {
+        this.$router.push("/OfficeHomeEvents");
       }
+    },
 
-      else if (this.usernamenako=="admin",this.password=="admin"){
-
-        this.$router.push('/OfficeHomeEvents'); 
-
-      }
-
-  },
-
-  /*    goToFormTwo() {
+    /*    goToFormTwo() {
         this.$router.push('/HtmlQrCodes');
       }  */
-      
-    }
-  };
-  </script>
+  },
+};
+</script>
   
   <style scoped>
-  .login-form {
-    max-width: 400px;
-    margin: 0 auto;
+.login-form {
+  max-width: 100%;
+  margin: 0 auto;
 
-    position: absolute;
+  position: absolute;
   left: 50%;
   top: 50%;
   width: 400px;
-  transform: translate(-50%,-50%);
+  transform: translate(-50%, -50%);
 
-  border-radius: 20px;
+  border-radius: 10px;
   box-shadow: 10px 10px 15px rgba(49, 47, 47, 0.15);
+}
 
-
-  }
-
-  .login-title {
-    text-align: center;
-  }
- 
-  </style>
+.login-title {
+  text-align: center;
+}
+</style>
   
